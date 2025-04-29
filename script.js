8104081346dd37ed830743f706daec85d0540173
Mark Christian Acana <markchristianacana@gmail.com>
2025-04-29 22:59:03 +0800

    Updated repository with latest changes

---
+++ script.js
@@ -0,0 +1,8 @@
+<script>
+    const menuToggle = document.getElementById('menu-toggle');
+    const nav = document.querySelector('nav');
+
+    menuToggle.addEventListener('click', () => {
+        nav.classList.toggle('active');
+    });
+</script>
